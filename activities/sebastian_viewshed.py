d0c981d55c7229d9b4addddbf866805959eb8551
JuanSebNieto <jsnietol@ncsu.edu>
2026-04-21 14:33:11 -0400

    Add .py and .json files to generate contours. (#1)

---
+++ activities/sebastian_viewshed.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+def run_viewshed(scanned_elev, env, points=None, **kwargs):
+
+    if not points:
+        points = "points"
+        import analyses
+
+        analyses.change_detection(
+            "scan_saved",
+            scanned_elev,
+            points,
+            height_threshold=[10, 100],
+            cells_threshold=[5, 50],
+            add=True,
+            max_detected=5,
+            debug=True,
+            env=env,
+        )
+
+    data = (
+        gs.read_command(
+            "v.out.ascii",
+            input=points,
+            type="point",
+            format="point",
+            separator="comma",
+            env=env,
+        )
+        .strip()
+        .splitlines()
+    )
+
+    if len(data) < 2:
+        # For the cases when the analysis expects at least 2 points, we check the
+        # number of points and return from the function if there is less than 2
+        # points. (No points is a perfectly valid state in Tangible Landscape,
+        # so we need to deal with it here.)
+        return
+
+    point = data[0]
+    x, y = [float(p) for p in point.split(",")][:2]
+
+    gs.run_command(
+        "r.viewshed",
+        input=scanned_elev,
+        output="viewshed",
+        coordinates=f"{x},{y}",
+        env=env,
+    )
+
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    elev_resampled = "elev_resampled"
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+
+    points = "points"
+    gs.write_command(
+        "v.in.ascii",
+        flags="t",
+        input="-",
+        output=points,
+        separator="comma",
+        stdin="638432,220382\n638621,220607",
+        env=env,
+    )
+
+    # Call the analysis.
+    run_viewshed(scanned_elev=elev_resampled, env=env, points=points)
+
+
+if __name__ == "__main__":
+    main()
